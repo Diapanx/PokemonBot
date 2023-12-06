7a96bfd162b8a6389ca7ca6f67b812a3367e4b4a
Yuhan Yang <103678967+xftudou@users.noreply.github.com>
2023-12-06 00:27:10 +0000

    saving changes to checkout merged files from main branch

--- src/main/java/edu/northeastern/cs5500/starterbot/command/TradeCommand.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/TradeCommand.java
@@ -23,114 +23,137 @@ import org.bson.types.ObjectId;
 @Slf4j
 public class TradeCommand implements SlashCommandHandler {
 
-    static final String NAME = "trade";
+        static final String NAME = "trade";
 
-    @Inject
-    TradeOfferController tradeOfferController;
-    @Inject
-    PokemonController pokemonController;
-    @Inject
-    TrainerController trainerController;
-    @Inject
-    PokedexController pokedexController;
+        @Inject
+        TradeOfferController tradeOfferController;
+        @Inject
+        PokemonController pokemonController;
+        @Inject
+        TrainerController trainerController;
+        @Inject
+        PokedexController pokedexController;
 
-    @Inject
-    public TradeCommand() {
-        // Defined public and empty for Dagger injection
-    }
-
-    @Override
-    @Nonnull
-    public String getName() {
-        return NAME;
-    }
-
-    @Override
-    @Nonnull
-    public CommandData getCommandData() {
-        return Commands.slash(getName(), "perform trading actions between users")
-                .addSubcommands(
-                        new SubcommandData("new", "Create a new trade")
-                                .addOption(
-                                        OptionType.STRING,
-                                        "pokemon-name",
-                                        "The pokemon user wants to trade.",
-                                        true),
-                        (new SubcommandData("list", "View all the open trades.")
-                                .addOption(
-                                        OptionType.USER,
-                                        "user",
-                                        "View user's open trades.(Enter their @mentions)",
-                                        true)));
-    }
-
-    @Override
-    public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event)
-            throws PokemonNotExistException {
-        log.info("event: /trade");
-        String trainerDiscordId = event.getMember().getId();
-        Trainer trainer = trainerController.getTrainerForMemberId(trainerDiscordId);
-
-        switch (event.getSubcommandName()) {
-            case "new":
-                // Objects.requireNonNull(trainer.getPokemonInventory());
-                String pokemonName = event.getOption("pokemon-name").getAsString();
-                ObjectId pokemonId = getPokemonFromInventory(trainer, pokemonName);
-                if (pokemonId == null) {
-                    event.reply(
-                            "You do not own the pokemon.\nPlease enter a valid pokemon for trade.")
-                            .queue();
-                    break;
-                }
-                tradeOfferController.createNewOffering(
-                        trainer, pokemonController.getPokemonById(pokemonId));
-
-                event.reply(
-                        String.format(
-                                "Player <@%s> has listed %s for trade.",
-                                trainerDiscordId, pokemonName))
-                        .queue();
-                break;
-            case "list":
-                String mentionedUserId = event.getOption("user").getAsMember().getId();
-                Trainer mentionedTrainer = trainerController.getTrainerForMemberId(trainerDiscordId);
-
-                StringBuilder avaliableTrades = new StringBuilder();
-                avaliableTrades.append(String.format("Player <@%s>'s Open Trades:\n", trainerDiscordId));
-                for (TradeOffer openTrade : tradeOfferController.getOpenTradesByTrainer(mentionedTrainer)) {
-                    Pokemon pokemonForTrade = pokemonController.getPokemonById(openTrade.getPokemonId());
-                    avaliableTrades
-                            .append(
-                                    "     " + pokedexController.getPokemonSpeciesByNumber(
-                                            pokemonForTrade.getPokedexNumber()).getName())
-                            .append("\n");
-                }
-                event.reply(avaliableTrades.toString()).setEphemeral(true).queue();
-
-                break;
-            default:
-                event.reply("Invalid subcommand").setEphemeral(true).queue();
+        @Inject
+        public TradeCommand() {
+                // Defined public and empty for Dagger injection
         }
-    }
 
-    // @Override
-    // public void onButtonInteraction(@Nonnull ButtonInteractionEvent event) {
-    // }
-
-    private ObjectId getPokemonFromInventory(
-            @Nonnull Trainer trainer, @Nonnull String pokemonName) {
-        List<ObjectId> trainerInventory = trainer.getPokemonInventory();
-        PokemonSpecies species = pokedexController.getSpeciesByName(pokemonName);
-        ObjectId resultPokemonId = null;
-        for (ObjectId pokemonId : trainerInventory) {
-            if (pokemonController
-                    .getPokemonById(pokemonId)
-                    .getPokedexNumber()
-                    .equals(species.getPokedexNumber())) {
-                resultPokemonId = pokemonId;
-                break;
-            }
+        @Override
+        @Nonnull
+        public String getName() {
+                return NAME;
         }
-        return resultPokemonId;
-    }
+
+        @Override
+        @Nonnull
+        public CommandData getCommandData() {
+                return Commands.slash(getName(), "perform trading actions between users")
+                                .addSubcommands(
+                                                new SubcommandData("new", "Create a new trade")
+                                                                .addOption(
+                                                                                OptionType.STRING,
+                                                                                "pokemon-name",
+                                                                                "The pokemon user wants to trade.",
+                                                                                true),
+                                                new SubcommandData("view", "View all the open trades.")
+                                                                .addOption(
+                                                                                OptionType.USER,
+                                                                                "user",
+                                                                                "View user's open trades.(Enter their @mentions)",
+                                                                                true)
+                                                                .addOption(OptionType.STRING, "all",
+                                                                                "View all open trades.", true),
+                                                new SubcommandData("with", "View all the open trades.")
+                                                                .addOption(
+                                                                                OptionType.USER,
+                                                                                "user",
+                                                                                "User you want to trade with.(Enter their @mentions)",
+                                                                                true)
+                                                                .addOption(
+                                                                                OptionType.STRING,
+                                                                                "your-pokemon",
+                                                                                "Enter your pokmon you want to trade with.)",
+                                                                                true)
+                                                                .addOption(
+                                                                                OptionType.STRING,
+                                                                                "their-pokemon",
+                                                                                "Enter the pokmon you want to trade for.)",
+                                                                                true));
+        }
+
+        @Override
+        public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event)
+                        throws PokemonNotExistException {
+                log.info("event: /trade");
+                String trainerDiscordId = event.getMember().getId();
+                Trainer trainer = trainerController.getTrainerForMemberId(trainerDiscordId);
+
+                switch (event.getSubcommandName()) {
+                        case "new":
+                                String pokemonName = event.getOption("pokemon-name").getAsString();
+                                ObjectId pokemonId = getPokemonFromInventory(trainer, pokemonName);
+                                if (pokemonId == null) {
+                                        event.reply(
+                                                        "You do not own the pokemon.\nPlease enter a valid pokemon for trade.")
+                                                        .queue();
+                                        break;
+                                }
+                                tradeOfferController.createNewOffering(
+                                                trainer, pokemonController.getPokemonById(pokemonId));
+
+                                event.reply(
+                                                String.format(
+                                                                "Player <@%s> has listed %s for trade.",
+                                                                trainerDiscordId, pokemonName))
+                                                .queue();
+                                break;
+                        case "view":
+                                String mentionedUserId = event.getOption("user").getAsMember().getId();
+                                Trainer mentionedTrainer = trainerController.getTrainerForMemberId(trainerDiscordId);
+
+                                StringBuilder avaliableTrades = new StringBuilder();
+                                avaliableTrades.append(
+                                                String.format("Player <@%s>'s Open Trades:\n", trainerDiscordId));
+                                for (TradeOffer openTrade : tradeOfferController
+                                                .getOpenTradesByTrainer(mentionedTrainer)) {
+                                        Pokemon pokemonForTrade = pokemonController
+                                                        .getPokemonById(openTrade.getPokemonId());
+                                        avaliableTrades
+                                                        .append(
+                                                                        "     "
+                                                                                        + pokedexController
+                                                                                                        .getPokemonSpeciesByNumber(
+                                                                                                                        pokemonForTrade.getPokedexNumber())
+                                                                                                        .getName())
+                                                        .append("\n");
+                                }
+                                event.reply(avaliableTrades.toString()).setEphemeral(true).queue();
+                                break;
+                        case "with":
+                                String userPokemonName = event.getOption("your-pokemon").getAsString();
+                                String tradePokemonName = event.getOption("their-pokemon").getAsString();
+                                event.reply("Not yet finished").setEphemeral(true).queue();
+                                break;
+                        default:
+                                event.reply("Invalid subcommand").setEphemeral(true).queue();
+                }
+        }
+
+        private ObjectId getPokemonFromInventory(
+                        @Nonnull Trainer trainer, @Nonnull String pokemonName) {
+                List<ObjectId> trainerInventory = trainer.getPokemonInventory();
+                PokemonSpecies species = pokedexController.getSpeciesByName(pokemonName);
+                ObjectId resultPokemonId = null;
+                for (ObjectId pokemonId : trainerInventory) {
+                        if (pokemonController
+                                        .getPokemonById(pokemonId)
+                                        .getPokedexNumber()
+                                        .equals(species.getPokedexNumber())) {
+                                resultPokemonId = pokemonId;
+                                break;
+                        }
+                }
+                return resultPokemonId;
+        }
 }
