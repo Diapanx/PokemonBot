061d64c67e1bd9a57381dc73d12d04ac48bb638c
Diapanx <64809110+Diapanx@users.noreply.github.com>
2023-12-07 17:09:18 +0000

    Change file path in `loadPokemonConfigs()`

--- src/main/java/edu/northeastern/cs5500/starterbot/controller/PokedexController.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/controller/PokedexController.java
@@ -40,7 +40,7 @@ public class PokedexController {
             // Create a FileReader to read the JSON file
             FileReader reader =
                     new FileReader(
-                            "/workspaces/bot-bear/src/main/java/edu/northeastern/cs5500/starterbot/resources/pokedex.json");
+                            "src/main/java/edu/northeastern/cs5500/starterbot/resources/pokedex.json");
 
             return gson.fromJson(reader, listType);
 
@@ -86,39 +86,6 @@ public class PokedexController {
         return builder.build();
     }
 
-    // @Nonnull
-    // public PokemonSpecies getPokemonSpeciesByNumber(int pokedexNumber) {
-    //     PokemonSpeciesBuilder builder = PokemonSpecies.builder();
-    //     builder.pokedexNumber(pokedexNumber);
-
-    //     switch (pokedexNumber) {
-    //         case 1: // Bulbasaur
-    //             builder.name("Bulbasaur");
-    //             builder.types(PokemonType.getSingleTypeArray(PokemonType.GRASS));
-    //             builder.imageUrl("https://placehold.co/256x256/green/white.png?text=Bulbasaur");
-    //             break;
-    //         case 4: // Charmander
-    //             builder.name("Charmander");
-    //             builder.types(PokemonType.getSingleTypeArray(PokemonType.FIRE));
-    //             builder.imageUrl("https://placehold.co/256x256/red/white.png?text=Charmander");
-    //             break;
-    //         case 7: // Squirtle
-    //             builder.name("Squirtle");
-    //             builder.types(PokemonType.getSingleTypeArray(PokemonType.WATER));
-    //             builder.imageUrl("https://placehold.co/256x256/blue/white.png?text=Squirtle");
-    //             break;
-    //         case 19: // Rattata
-    //             builder.name("Rattata");
-    //             builder.types(PokemonType.getSingleTypeArray(PokemonType.NORMAL));
-    //             builder.imageUrl("https://placehold.co/256x256.png?text=Rattata");
-    //             break;
-    //         default:
-    //             throw new IllegalStateException();
-    //     }
-
-    //     return Objects.requireNonNull(builder.build());
-    // }
-
     public PokemonSpecies getSpeciesByName(String name) {
         PokemonConfig foundConfig = null;
         for (PokemonConfig config : pokemonConfigs) {
@@ -134,35 +101,4 @@ public class PokedexController {
 
         return buildPokemonSpecies(foundConfig);
     }
-
-    // public PokemonSpecies getSpeciesByName(String name) {
-    //     PokemonSpeciesBuilder builder = PokemonSpecies.builder();
-    //     builder.name(name);
-    //     switch (name) {
-    //         case "Bulbasaur": // Bulbasaur
-    //             builder.pokedexNumber(1);
-    //             builder.types(PokemonType.getSingleTypeArray(PokemonType.GRASS));
-    //             builder.imageUrl("https://placehold.co/256x256/green/white.png?text=Bulbasaur");
-    //             break;
-    //         case "Charmander": // Charmander
-    //             builder.pokedexNumber(4);
-    //             builder.types(PokemonType.getSingleTypeArray(PokemonType.FIRE));
-    //             builder.imageUrl("https://placehold.co/256x256/red/white.png?text=Charmander");
-    //             break;
-    //         case "Squirtle": // Squirtle
-    //             builder.pokedexNumber(7);
-    //             builder.types(PokemonType.getSingleTypeArray(PokemonType.WATER));
-    //             builder.imageUrl("https://placehold.co/256x256/blue/white.png?text=Squirtle");
-    //             break;
-    //         case "Rattata": // Rattata
-    //             builder.pokedexNumber(19);
-    //             builder.types(PokemonType.getSingleTypeArray(PokemonType.NORMAL));
-    //             builder.imageUrl("https://placehold.co/256x256.png?text=Rattata");
-    //             break;
-    //         default:
-    //             throw new IllegalStateException();
-    //     }
-
-    //     return Objects.requireNonNull(builder.build());
-    // }
 }
